444a360387c06c13bb6fcab182f58eeed12d7a31
eugenedrvnk <eugenedrvnk@gmail.com>
2019-09-05 16:27:59 +0300

    1

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '405319146759777', '350475a5f35e7ea70d3659fa58f56a3e'
+  provider :facebook, '491409018257450', '8627d1688e929b90a6b919abefce44f8'
   provider :linkedin, '77apwp59c4g58f', '0Zu8jK0n9GNfpG4E'
 end
