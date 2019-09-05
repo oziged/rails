e92a698e0332c92117fb69db8a5fef63f5cd718f
eugenedrvnk <eugenedrvnk@gmail.com>
2019-09-05 14:56:23 +0300

    mail added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.default_url_options = { :host => "https://whispering-scrubland-19020.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
